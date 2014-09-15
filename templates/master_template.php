93571f057fa7c299d77bce1bd8bb4bc606980da6
kcrolin <carolin.karing@khk.ee>
2014-09-15 15:21:36 +0100

    Fixed nav functionality

--- templates/master_template.php
+++ templates/master_template.php
@@ -65,9 +65,9 @@
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="../navbar/">Default</a></li>
 				<li><a href="../navbar-static-top/">Static top</a></li>
-                <li><a href="/blog/login">Login</a></li>
-                <li><a href="/blog/logout">Logout</a></li>
-				<li class="active"><a href="./">Fixed top</a></li>
+                <li><a href="<?= BASE_URL ?><?= $auth->logged_in == true? 'Logout' : 'Login' ?>"><?=$auth->logged_in == true? 'Logout' : 'Login' ?></a></li>
+
+                <li class="active"><a href="./">Fixed top</a></li>
 			</ul>
 		</div><!--/.nav-collapse -->
 	</div>
