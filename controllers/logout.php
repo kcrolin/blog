1575cb0bc3579829e7a0c4bf9e585e6ef7a055b8
kcrolin <carolin.karing@khk.ee>
2014-09-15 12:51:43 +0100

    Added login/logout functionality

--- controllers/logout.php
+++ controllers/logout.php
@@ -1,14 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hennotaht
- * Date: 7/29/13
- * Time: 21:48
- */
+<?php class logout extends Controller
+{ function index(){ session_destroy(); header('Location: '.BASE_URL); } }
 
-class logout extends Controller {
-	function index(){
-		session_destroy();
-		header('Location: '.BASE_URL);
-	}
-} 
+?>
