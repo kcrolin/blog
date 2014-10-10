a6f9d3bd1bb68ad2990fd74d5e7874084a54fca4
kcrolin <carolin.karing@khk.ee>
2014-10-10 07:19:44 +0100

    Create posts.php

---
+++ controllers/posts.php
@@ -0,0 +1,27 @@
+<?php
+class posts extends controller{
+
+    function index() {
+        $this->posts = get_all("SELECT * FROM post");
+        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
+        foreach ($_tags as $tag) { $this->tags[$tag['post_id']][] = $tag['tag_name']; } }
+
+    function index_ajax() {
+        echo "\$_POST:<br>"; var_dump($_POST); }
+
+    function index_post() {
+        echo "\$_POST:<br>"; var_dump($_POST);
+        $data = $_POST['data']; $data['post_id'] = $this->params[0]; $data['user_id'] = '1';
+        insert('post', $data); }
+
+    function view() {
+        $post_id = $this ->params[0];
+        $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+        $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+        $this->comments = get_all("SELECT * FROM comment NATURAL JOIN post WHERE post_id='$post_id'"); }
+
+    function view_post() {
+        $data = $_POST['data'];
+        $data['post_id'] = $this->params[0];
+        $data['comment_author'] = $_SESSION['user_id'];
+        INSERT('comment',$data); } } ?>
